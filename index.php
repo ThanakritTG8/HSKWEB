6aaf8e65f60e04c2efe5cd643b1c03fff481597d
karin22 <not.karin@hotmail.com>
2021-09-15 17:12:00 +0700

    เพิ่ม meta tag

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Primary Meta Tags -->
+    <title>HSK</title>
+    <meta name="title" content="HSK">
+    <meta name="description" content="บทเรียนและบทดสอบHSK online">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://chochiang.com/chinyhot_HSK13/">
+    <meta property="og:title" content="HSK">
+    <meta property="og:description" content="บทเรียนและบทดสอบHSK online">
+    <meta property="og:image" content="https://chochiang.com/chinyhot_HSK13/img/Logo.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://chochiang.com/chinyhot_HSK13/">
+    <meta property="twitter:title" content="HSK">
+    <meta property="twitter:description" content="บทเรียนและบทดสอบHSK online">
+    <meta property="twitter:image" content="https://chochiang.com/chinyhot_HSK13/img/Logo.png">
+</head>
+<script>
+    window.location.replace('Login.php')
+</script>
+
+</html>
