b982c7800f1ac86207adef5c3c79decb844c88a6
ThanakritTG8 <tgrock1997@gmail.com>
2021-11-09 23:32:29 +0700

    add student tab

--- pages/admin/teacher.php
+++ pages/admin/teacher.php
@@ -86,6 +86,9 @@
                   <li class="nav-item" role="presentation">
                       <a class="nav-link" id="School-tab" data-toggle="tab" href="#School" role="tab" aria-controls="School" aria-selected="false">School</a>
                   </li>
+                  <li class="nav-item" role="presentation">
+                      <a class="nav-link" id="student-tab" data-toggle="tab" href="#student" role="tab" aria-controls="student" aria-selected="false">student</a>
+                  </li>
 
               </ul>
               <div class="tab-content">
@@ -149,35 +152,35 @@
                                     while ($row = mysqli_fetch_assoc($result)) {
 
                                         echo
-                                        ' <tr>
-      <td>' . $row['SchoolName'] . '</td>
-      <td><i  class="fas fa-trash icon-delete" data-toggle="modal" data-target="#modal-delete' . $row['SchoolID'] . '"></i></td>
-      <!-- /// Modal Delete -->
-      <div class="modal fade" id="modal-delete' . $row['SchoolID'] . '" tabindex="-1" aria-labelledby="modal-deleteLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
+                                        '<tr>
+                                            <td>' . $row['SchoolName'] . '</td>
+                                            <td><i  class="fas fa-trash icon-delete" data-toggle="modal" data-target="#modal-delete' . $row['SchoolID'] . '"></i></td>
+                                            <!-- /// Modal Delete -->
+                                            <div class="modal fade" id="modal-delete' . $row['SchoolID'] . '" tabindex="-1" aria-labelledby="modal-deleteLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
 
-                  <form action="Delete.php" class="needs-validation" novalidate autocomplete="off" method="post">
-                  <div class="modal-body" >
-                     คุณต้องการที่จะลบ ?
-                      <hr>
-                     <div class="d-flex justify-content-center">
-                        <span>โรงเรียน ' . $row['SchoolName'] . '</span> 
-                      </div>
-                  </div>
-                  <div class="modal-footer">
-                        <input type="hidden" name="sh_delete" value="' . $row['SchoolID'] . '">
-                      <button type="submit" class="btn btn-primary">ยืนยัน</button>
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
-                     
-                  </div>
-                  </form>
-              </div>
-          </div>
-      </div>
-      <!-- //// -->
-   
-  </tr>';
+                                                        <form action="Delete.php" class="needs-validation" novalidate autocomplete="off" method="post">
+                                                        <div class="modal-body" >
+                                                            คุณต้องการที่จะลบ ?
+                                                            <hr>
+                                                            <div class="d-flex justify-content-center">
+                                                                <span>โรงเรียน ' . $row['SchoolName'] . '</span> 
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                                <input type="hidden" name="sh_delete" value="' . $row['SchoolID'] . '">
+                                                            <button type="submit" class="btn btn-primary">ยืนยัน</button>
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+                                                            
+                                                        </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- //// -->
+                                        
+                                        </tr>';
                                     }
                                     ?>
 
@@ -273,39 +276,39 @@
 
                                         echo
                                         ' <tr>
-      <td>' . $row['Tname'] . ' ' . $row['TLname'] . '</td>
-      <td>' . $row['TUsername'] . '</td>
-      <td>' . $row['TPassword'] . '</td>
-      <td>' . $row['SchoolName'] . '</td>
-      <td><i class="fas fa-trash icon-delete" data-toggle="modal" data-target="#modal-delete' . $row['TID'] . '"></i></td>
-      <!-- /// Modal Delete -->
-      <div class="modal fade" id="modal-delete' . $row['TID'] . '" tabindex="-1" aria-labelledby="modal-deleteLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
+                                            <td>' . $row['Tname'] . ' ' . $row['TLname'] . '</td>
+                                            <td>' . $row['TUsername'] . '</td>
+                                            <td>' . $row['TPassword'] . '</td>
+                                            <td>' . $row['SchoolName'] . '</td>
+                                            <td><i class="fas fa-trash icon-delete" data-toggle="modal" data-target="#modal-delete' . $row['TID'] . '"></i></td>
+                                            <!-- /// Modal Delete -->
+                                            <div class="modal fade" id="modal-delete' . $row['TID'] . '" tabindex="-1" aria-labelledby="modal-deleteLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
 
-                  <div class="modal-body">
-                      คุณต้องการที่จะลบ ?
-                      <hr>
-                    
-                      <p>Account : ' . $row['Tname'] . ' ' . $row['TLname'] . ' </p>
-                      <p>โรงเรียน ' . $row['SchoolName'] . '  </p>
-                     
-                    
-                  </div>
-                  <div class="modal-footer">
-                  <form action="Delete.php" method="post">
-                       <input type="hidden" name="delete" value="' . $row['TID'] . '">
-                        <button type="submit" class="btn btn-primary">ยืนยัน</button>
-                         <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
-                      
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <!-- //// -->
-   
-  </tr>';
+                                                        <div class="modal-body">
+                                                            คุณต้องการที่จะลบ ?
+                                                            <hr>
+                                                            
+                                                            <p>Account : ' . $row['Tname'] . ' ' . $row['TLname'] . ' </p>
+                                                            <p>โรงเรียน ' . $row['SchoolName'] . '  </p>
+                                                            
+                                                            
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                        <form action="Delete.php" method="post">
+                                                            <input type="hidden" name="delete" value="' . $row['TID'] . '">
+                                                                <button type="submit" class="btn btn-primary">ยืนยัน</button>
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+                                                            
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- //// -->
+                                        
+                                        </tr>';
                                     }
                                     ?>
 
@@ -314,6 +317,85 @@
                           </table>
                       </div>
                   </div>
+
+
+                  <!-- /////Student -->
+                  <div class="tab-pane fade show active" id="student" role="tabpanel" aria-labelledby="student-tab">
+                      <h5 class="card-title">Student Account</h5>
+
+                      <div class="header d-flex justify-content-between">
+                          <div class="btn btn-success-teacher btn-success" data-toggle="modal" data-target="#modal-create">เพิ่ม<i class="fas fa-plus-circle" style=" margin-left: 5px;"></i></div>
+                          <div class="d-flex justify-content-end">
+                              <input class="form-control p-2" id="studentInput" type="text" placeholder="Search..">
+                          </div>
+                      </div>
+
+                      <!-- //// -->
+                      <div class="student-table">
+                          <table class="table table-bordered">
+                              <thead style=" background-color: #ececec;">
+                                  <tr>
+                                      <th scope="col">ชื่อ-นามสกุล</th>
+                                      <th scope="col">Username</th>
+                                      <th scope="col">password</th>
+                                      <th scope="col">School</th>
+
+                                      <th scope="col">ลบ</th>
+                                  </tr>
+                              </thead>
+                              <tbody id="studentTable">
+                                  <?php
+
+                                    include('../../database/database.php');
+                                    $query = "SELECT * FROM Student INNER JOIN School ON Student.SchoolID = School.SchoolID ";
+                                    $result = mysqli_query($conn, $query);
+
+                                    while ($row = mysqli_fetch_assoc($result)) {
+
+                                        echo
+                                        ' <tr>
+                                            <td>' . $row['Sname'] . ' ' . $row['SLname'] . '</td>
+                                            <td>' . $row['SUsername'] . '</td>
+                                            <td>' . $row['Spassword'] . '</td>
+                                            <td>' . $row['SchoolName'] . '</td>
+                                            <td><i class="fas fa-trash icon-delete" data-toggle="modal" data-target="#modal-delete' . $row['SID'] . '"></i></td>
+                                            <!-- /// Modal Delete -->
+                                            <div class="modal fade" id="modal-delete' . $row['SID'] . '" tabindex="-1" aria-labelledby="modal-deleteLabel" aria-hidden="true">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
+
+                                                        <div class="modal-body">
+                                                            คุณต้องการที่จะลบ ?
+                                                            <hr>
+                                                            
+                                                            <p>Account : ' . $row['Sname'] . ' ' . $row['SLname'] . ' </p>
+                                                            <p>โรงเรียน ' . $row['SchoolName'] . '  </p>
+                                                            
+                                                            
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                        <form action="Delete.php" method="post">
+                                                            <input type="hidden" name="delete" value="' . $row['SID'] . '">
+                                                                <button type="submit" class="btn btn-primary">ยืนยัน</button>
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+                                                            
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- //// -->
+                                        
+                                        </tr>';
+                                    }
+                                    ?>
+
+
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+
               </div>
 
 
@@ -330,6 +412,12 @@
                   $("#schoolInput").on("keyup", function() {
                       var value = $(this).val().toLowerCase();
                       $("#schoolTable tr").filter(function() {
+                          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                      });
+                  });
+                  $("#studentInput").on("keyup", function() {
+                      var value = $(this).val().toLowerCase();
+                      $("#studentTable tr").filter(function() {
                           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
                       });
                   });
